8f416ee48dd271d9bbd52a0570dfc05f21e5e051
gaahbiomedical-dev <gaahbiomedical@gmail.com>
2025-11-06 13:58:41 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/transactions': {
-        target: 'http://localhost:8000', // backend API URL
-        changeOrigin: true
-      },
-      '/books': {
-        target: 'http://localhost:8000', // backend API URL
-        changeOrigin: true
-      }
-    }
-  }
+  base: '/gaah-bookkeeping-render/',  // IMPORTANT: repo name as base path
+  plugins: [vue()]
 })
