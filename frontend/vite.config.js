24b7f054313c8d44f5db754c7f20ea634f4968af
gaahbiomedical-dev <gaahbiomedical@gmail.com>
2025-11-06 14:10:51 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/gaah-bookkeeping-render/',  // IMPORTANT: repo name as base path
+  base: '/gaah-bookkeeping-render/', 
   plugins: [vue()]
 })
