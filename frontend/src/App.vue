9a6febd3d406cb4d7afbeefa88e4b8cbd86de4b4
gaahbiomedical-dev <gaahbiomedical@gmail.com>
2025-11-06 15:39:17 +0500

    Delete frontend directory

--- frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>GAAH Bookkeeping (Prototype)</h1>
-    <router-view />
-  </div>
-</template>
-<script>export default {}</script>
-<style>body{font-family:Arial;padding:20px}</style>
--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/gaah-bookkeeping-render/', 
-  plugins: [vue()]
-})
