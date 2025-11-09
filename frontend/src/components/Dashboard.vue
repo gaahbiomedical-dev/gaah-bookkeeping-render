5be37817a8576c1d4e4bd60313c766ad448e075e
gaahbiomedical-dev <gaahbiomedical@gmail.com>
2025-11-09 10:17:00 +0500

    Update Dashboard.vue

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -3,35 +3,6 @@
     <h2>Dashboard</h2>
     <p>Books</p>
     <ul>
-<<<<<<< HEAD
-      <li v-for="b in books" :key="b">
-        <router-link :to="`/books/${b}`">{{ b }}</router-link>
-      </li>
-    </ul>
-  </div>
-</template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      books: []
-    }
-  },
-  async created() {
-    try {
-      const res = await axios.get('/books')
-      this.books = res.data.map(b => b.name)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-}
-</script>
-
-=======
       <li v-for=\"b in books\" :key=\"b\"><router-link :to=\"`/books/${b}`\">{{b}}</router-link></li>
     </ul>
   </div>
@@ -47,5 +18,7 @@ export default {
     }catch(e){ console.log(e) }
   }
 }
+</script>
+
 </script>
 >>>>>>> 1c071df926c702b72a64ef539685c19e319a01de
