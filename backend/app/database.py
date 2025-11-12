f85593703bae118a8e336bf1cc498a81084b6fa1
gaahbiomedical-dev <gaahbiomedical@gmail.com>
2025-11-12 11:08:21 +0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,11 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://gaah_bookkeeping_db_user:tnpETXEeKhDJ7tbeP7nuHE2uAxsqTlZi@dpg-d461tushg0os73e9g1h0-a/gaah_bookkeeping_db").replace("postgres://", "postgresql://", 1)
-connect_args = {}
-if DATABASE_URL.startswith('sqlite:///'):
-    connect_args = {'check_same_thread': False}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./gaah.db")
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
